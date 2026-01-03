c82075659800bbc2d90abc97b0a7f195b0e82729
LzK <101060286+001zk@users.noreply.github.com>
2026-01-03 22:16:39 +0000

    update teste

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-function checkVisibility() {
-    const elements = document.querySelectorAll('*');
-    elements.forEach(element => {
-        const rect = element.getBoundingClientRect();
-        if (rect.top <= window.innerHeight && rect.bottom >= 0) {
-            if (!element.classList.contains('visible')) {
-                element.classList.add('visible');
-            }
-        } else {
-            if (element.classList.contains('visible')) {
-                element.classList.remove('visible');
-            }
+const elements = document.querySelectorAll('.fade');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
         }
     });
-}
+}, {
+    threshold: 0.2
+});
 
-window.addEventListener('scroll', checkVisibility);
-window.addEventListener('load', checkVisibility);
-checkVisibility();
-
-
+elements.forEach(el => observer.observe(el));
